ae354c37151222fe51066d30e6406a4bc1b3c810
FredrikOlssons <fredrik_olssons@live.se>
2022-05-31 15:58:15 +0200

    Revert "hero"
    This reverts commit 8a32deeabe9111a5981243d40ab62bb818f230f7.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,9 @@
 import { Container, Box } from "@mui/material";
-import React, { CSSProperties } from "react";
+import React from "react";
 //mport LazyHero from 'react-lazy-hero'; 
 import { FC } from "react";
 import { CssBaseline } from "@material-ui/core";
 import { Colors } from '../data/colors';
-import lady from '../assets/lady.jpg'
-import { width } from "@mui/system";
-import { fontSize, fontStyle } from "@material-ui/core/node_modules/@material-ui/system";
-import '../style/font.css'
 
 
 const Hero: FC = () => {
@@ -15,35 +11,11 @@ const Hero: FC = () => {
         <React.Fragment>
         <CssBaseline />
         <Container>
-          <div style={heroImg}><img style={img} src={lady}/><h1 style={heroTitle}>Company Name</h1>
-          </div>
+          <Box  component="img" sx={{ bgcolor: Colors.primary, width: '100%', height: '50vh', border: '1px solid black' }}
+          src="../assets/nastyGrandma.jpg" /> 
         </Container>
       </React.Fragment>
     )
 }
-
-const heroImg: CSSProperties = {
-    paddingTop: '40px',
-    width: '80vw',
-    display: 'flex',
-    justifyContent: 'center',
-}
-
-const img: CSSProperties = {
-width: '100%'
-}
-
-const heroTitle: CSSProperties = {
-    position: 'absolute',
-    left: '5%',
-    top: '10%',
-    fontSize: '20px',
-    fontFamily: 'Frijole'
-   
-    }
-
-
-
-
 
 export default Hero
