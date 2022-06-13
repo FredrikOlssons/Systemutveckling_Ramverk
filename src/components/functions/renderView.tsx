3b94a62acb1593ac7805e32e01cf4bf4d94612cd
Amïna <88747914+AminaHallam@users.noreply.github.com>
2022-06-13 09:51:44 +0200

    worked-with-MediaQuery-and-cartProvider

--- src/components/functions/renderView.tsx
+++ src/components/functions/renderView.tsx
@@ -2,21 +2,19 @@ import { FC} from 'react';
 import { Link, Routes, Route } from 'react-router-dom'; 
 import Content from '../allPages/StartPageContent';
 import SinglePage from '../allPages/singlePage';
-import Checkout from '../allPages/checkOut';
+import CheckOut from '../allPages/checkOut';
 import Admin from '../allPages/admin';
 
-interface Props{
-
-}
+interface Props{}
 
 
-const RenderView: FC<Props> = (props) => {
+const RenderView: React.FC<Props> = (props) => {
     return( 
 
         <Routes> 
             <Route path="/" element={ <Content /> } />
             <Route path="/:productId" element={ <SinglePage /> } />
-            <Route path="/checkOut" element={ <Checkout /> } />
+            <Route path="/checkOut" element={ <CheckOut /> } />
             <Route path="/admin" element={ <Admin /> } />
 
         </Routes>
